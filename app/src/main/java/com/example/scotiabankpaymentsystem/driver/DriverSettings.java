17ebcfd8a2cce54f55fca127a00a1e28038d78d2
长腿悦然 <paula@changtuiyuerandeMacBook-Pro.local>
2019-11-18 13:56:02 -0500

    perform their name on setting.xml for each driver

--- app/src/main/java/com/example/scotiabankpaymentsystem/driver/DriverSettings.java
+++ app/src/main/java/com/example/scotiabankpaymentsystem/driver/DriverSettings.java
@@ -2,11 +2,20 @@ package com.example.scotiabankpaymentsystem.driver;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.scotiabankpaymentsystem.R;
 import com.example.scotiabankpaymentsystem.data.ChangePassword;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 /**
  * A page that has Driver's settings.
  */
@@ -16,6 +25,17 @@ public class DriverSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_businessowner_settings);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        // Get the information of the current logged in user from database
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                String newNameString = dataSnapshot.child("Truck Driver").child(userID).child("Name").getValue(String.class);
+                TextView nameText = findViewById(R.id.ChangeName);
+                System.out.println("invoice check once");
+                nameText.setText(newNameString);
 
         //Checking if the changePassword in SBO's Settings page button has been pressed
         findViewById(R.id.password).setOnClickListener(v -> {
@@ -28,4 +48,7 @@ public class DriverSettings extends AppCompatActivity {
         Intent intent = new Intent(com.example.scotiabankpaymentsystem.driver.DriverSettings.this, ChangePassword.class);
         startActivity(intent);
     }
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });}
 }
