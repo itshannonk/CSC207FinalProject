1ed8b2152c76cec46f46332dbfc31058e63e1ec7
长腿悦然 <paula@changtuiyuerandeMacBook-Pro.local>
2019-11-18 01:25:28 -0500

    change coca_has xml invoce to textview

--- app/src/main/java/com/example/scotiabankpaymentsystem/cocacola/CocaColaSeeInvoices.java
+++ app/src/main/java/com/example/scotiabankpaymentsystem/cocacola/CocaColaSeeInvoices.java
@@ -1,12 +1,25 @@
 package com.example.scotiabankpaymentsystem.cocacola;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.scotiabankpaymentsystem.Invoice.Invoice;
 import com.example.scotiabankpaymentsystem.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
 
 public class CocaColaSeeInvoices extends AppCompatActivity {
+//    public static boolean testing = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
