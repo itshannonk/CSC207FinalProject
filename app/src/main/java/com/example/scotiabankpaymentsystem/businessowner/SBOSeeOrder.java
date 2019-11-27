ae0968e7a1c11db9ace912f8b0c1b7b792e9eb43
Grace Tsao <47228306+gracet9182@users.noreply.github.com>
2019-11-27 02:44:33 -0500

    implemented the ability for the user to pay their invoice that they select

--- app/src/main/java/com/example/scotiabankpaymentsystem/businessowner/SBOSeeOrder.java
+++ app/src/main/java/com/example/scotiabankpaymentsystem/businessowner/SBOSeeOrder.java
@@ -3,6 +3,8 @@ package com.example.scotiabankpaymentsystem.businessowner;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +18,7 @@ import com.example.scotiabankpaymentsystem.R;
 import com.example.scotiabankpaymentsystem.businessowner.home.SBOHomeActivity;
 
 import android.os.Handler;
+import android.widget.Toast;
 //import com.google.firebase.auth.FirebaseAuth;
 
 
@@ -27,26 +30,33 @@ import android.os.Handler;
 public class SBOSeeOrder extends AppCompatActivity {
     private static boolean testing = false;
     private String userID;
+    private String invoiceID;
+    private Button payButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_businessowner_seestatus_has);
+        // this will initiate the pay button
+        payButton = (Button)findViewById(R.id.Pay);
+        payButton.setOnClickListener(v -> changeBoolean());
 
+        //retrieving the information passed on from the previous activity
         Intent intent = getIntent();
         userID = intent.getStringExtra("userID");
-        String invoiceID = intent.getStringExtra("invoiceID");
-        System.out.println(userID);
-        System.out.println(invoiceID);
+        invoiceID = intent.getStringExtra("invoiceID");
+
         TextView invoiceText = findViewById(R.id.totalPrice);
         String tempForInvoice = "this is the userID: " + userID + "this is the invoice ID: " + invoiceID;
         invoiceText.setText(tempForInvoice);
 
         System.out.println("did it come here");
+        //api calls
         com.android.volley.RequestQueue ExampleRequestQueue = Volley.newRequestQueue(this);
         String url = "https://us-central1-csc207-tli.cloudfunctions.net/get_invoice_information?userID="+userID+"&invoiceID="+invoiceID;
         StringRequest ExampleStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
+                // setting the information in TextView according to the firebase
                 System.out.println("did it come here22222222" + response);
                 if(!(response.equals(""))) {
                     String[] invoiceIDs = response.split(",");
@@ -63,6 +73,12 @@ public class SBOSeeOrder extends AppCompatActivity {
                     TextView invoiceTextPaid = findViewById(R.id.Paid);
                     inputText ="Paid: " + invoiceIDs[2];
                     invoiceTextPaid.setText(inputText);
+                    System.out.println(invoiceIDs[2]+"prinout");
+                    System.out.println(invoiceIDs[2].getClass()+"classsssss");
+                    //checks if it has already been paid so it determines if the pay button should be there
+                    if((invoiceIDs[2]).toLowerCase().equals("true")){
+                        payButton.setVisibility(View.GONE);
+                    }
                     //showing the total price
                     TextView invoiceTextPrice = findViewById(R.id.totalPrice);
                     inputText ="Total Price " + invoiceIDs[3];
@@ -76,53 +92,33 @@ public class SBOSeeOrder extends AppCompatActivity {
             }
         });
         ExampleRequestQueue.add(ExampleStringRequest);
-        //DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-
-        // Get the information of the current logged in user from database
-        //databaseReference.addValueEventListener(new ValueEventListener() {
-           // @Override
-            //public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                //String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                //String newInvoiceString = dataSnapshot.child("Business Owner").child(userID).child("Invoices").getValue(String.class);
-//                TextView invoiceText = findViewById(R.id.Invoice);
-//                System.out.println("invoice check once");
-//                if(!(newInvoiceString.equals(""))){
-//                    invoiceText.setText(newInvoiceString);
-//                }
-//                if(!testing) {
-//                    //This is how you convert from a json file to an object
-//                    Gson gson = new Gson();
-//                    String json = newInvoiceString;
-//                    Invoice invoice = gson.fromJson(json, Invoice.class);
-//                    //we can now do methods around the invoices
-//                    invoice.setId(5);
-//                    invoice.getStatus().setIssued(true);
-//                    invoice.getStatus().setDelivered(true);
-//                    invoice.getStatus().setPaid(true);
-//                    //changing it back to an String to push back to the firebase
-//                    Gson gsonChanged = new Gson();
-//                    String mynewJSON = gsonChanged.toJson(invoice);
-//                    FirebaseDatabase database = FirebaseDatabase.getInstance("https://csc207-tli.firebaseio.com");
-//                    DatabaseReference roleDatabaseReference = database.getReference("Business Owner");
-//                    DatabaseReference myRef = roleDatabaseReference.child(FirebaseAuth.getInstance().getCurrentUser().getUid());
-//                    DatabaseReference RefToReplace = myRef.child("Invoices");
-//                    //slight delay for the display of the data to mimic realtime
-//                    new Handler().postDelayed(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            RefToReplace.setValue(mynewJSON);
-//                        }
-//                    }, 2000);
-//                    //testing boolean is for testing purposes so that it only executes this once.
-//                    testing = true;
-//                }
-            //}
-
-            //@Override
-            //public void onCancelled(@NonNull DatabaseError databaseError) {
-            //}
-        //});
     }
+    //changes the boolean for the payment
+    private void changeBoolean(){
+        com.android.volley.RequestQueue ExampleRequestQueue = Volley.newRequestQueue(this);
+        String url = "https://us-central1-csc207-tli.cloudfunctions.net/set_invoice_status?userid="+userID+"&invoiceid="+invoiceID+"&statustype="+"paid"+"&newvalue="+true;
+        StringRequest ExampleStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                System.out.println("did it come here for the pay button" + response);
+                TextView invoiceTextPaid = findViewById(R.id.Paid);
+                invoiceTextPaid.setText("Paid: True");
+                // makes the payment option invisible if it has been paid
+                payButton.setVisibility(View.GONE);
+                Toast.makeText(getApplicationContext(), "Your order has been processed.", Toast.LENGTH_LONG).show();
+
+            }
+        }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //This code is executed if there is an error.
+                Toast.makeText(getApplicationContext(), "Oops, there has been an error in the payment", Toast.LENGTH_LONG).show();
+            }
+        });
+        ExampleRequestQueue.add(ExampleStringRequest);
+
+    }
+    // for the back button
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
