ac621cb613a52883f5129a3c00e94459c0ba81ef
Grace Tsao <47228306+gracet9182@users.noreply.github.com>
2019-12-04 18:28:23 -0500

    Fixed the restart button for the small business owner

--- app/src/main/java/com/example/scotiabankpaymentsystem/businessowner/displayinvoice/SBODisplayInvoiceActivity.java
+++ app/src/main/java/com/example/scotiabankpaymentsystem/businessowner/displayinvoice/SBODisplayInvoiceActivity.java
@@ -141,6 +141,10 @@ public class SBODisplayInvoiceActivity extends AppCompatActivity implements SBOD
                 startActivity(newIntent);
                 finish();
                 break;
+
+            case R.id.refresh:
+                startSetInvoiceInfo();
+                break;
         }
         return true;
     }
