38823887c15beb59a4b91f23df793fa522d8b2ed
Grace Tsao <47228306+gracet9182@users.noreply.github.com>
2019-12-04 21:27:17 -0500

    Listener JavaDoc

--- app/src/main/java/com/example/scotiabankpaymentsystem/Listener.java
+++ app/src/main/java/com/example/scotiabankpaymentsystem/Listener.java
@@ -1,5 +1,8 @@
 package com.example.scotiabankpaymentsystem;
 
+/**
+ * This is the Listener parent class for SBO and Truck Driver
+ */
 public interface Listener {
     void onPageSuccess(String username);
 
