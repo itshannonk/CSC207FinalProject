c49035ceb3604827900cff87e6c9ec0bcfddc008
长腿悦然 <paula@changtuiyuerandeMacBook-Pro.local>
2019-12-04 19:36:11 -0500

    add java doc for SBO display invoice presenter

--- app/src/main/java/com/example/scotiabankpaymentsystem/businessowner/displayinvoice/SBODisplayInvoiceInteractor.java
+++ app/src/main/java/com/example/scotiabankpaymentsystem/businessowner/displayinvoice/SBODisplayInvoiceInteractor.java
@@ -12,20 +12,49 @@ public class SBODisplayInvoiceInteractor {
     }
 
     public interface onDisplayDataFinishedListener {
-        //successfully retrieves user information from database
+        /**
+         * successfully change pay boolean
+         */
         void onChangePaySuccess();
 
+        /**
+         * Change pay boolean has error
+         */
         void onChangePayError();
 
+        /**
+         * successfully retrieves user information from database
+         */
         void onInvoiceRetriveSuccess(String[] info);
 
+        /**
+         *
+         * @param userID id of user
+         * @param invoiceID id of invoice
+         * @param context the context of method that happening
+         */
         void onPay(String userID, String invoiceID, Context context);
     }
 
+    /**
+     * Change the Pay boolean in backend
+     *
+     * @param listener an interface inside  interactor
+     * @param userID   the userID of the current logged in user
+     * @param context context of the activity has happened
+     */
     void changePayBoolean(final onDisplayDataFinishedListener listener, final String userID, final String invoiceID, Context context) {
         APIFacadeInvoice.changePayBoolean(listener, userID, invoiceID, context);
     }
 
+    /**
+     * Retrieves the current invoice of the current logged in user from the database
+     *
+     * @param listener an interface inside  interactor
+     * @param userID the userID of the current logged in user
+     * @param invoiceID the id of invoice
+     * @param context context of the activity has happened
+     */
     void retrieveInvoice(final onDisplayDataFinishedListener listener, final String userID, final String invoiceID, Context context) {
         APIFacadeInvoice.retrieveInvoice(listener, userID, invoiceID, context);
 
