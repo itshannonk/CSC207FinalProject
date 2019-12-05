978b6aff40c5729b6b43ed71af02f3eb0a866187
长腿悦然 <paula@changtuiyuerandeMacBook-Pro.local>
2019-12-04 19:42:16 -0500

    add comment for SBODisplayInvoiceView

--- app/src/main/java/com/example/scotiabankpaymentsystem/businessowner/displayinvoice/SBODisplayInvoiceView.java
+++ app/src/main/java/com/example/scotiabankpaymentsystem/businessowner/displayinvoice/SBODisplayInvoiceView.java
@@ -6,13 +6,31 @@ package com.example.scotiabankpaymentsystem.businessowner.displayinvoice;
  */
 
 public interface SBODisplayInvoiceView {
+    /**
+     * Start to change pay boolean
+     */
     void navigateToPay();
 
+    /**
+     * Start display the retrieve invoice in the format we want
+     */
     void startSetInvoiceInfo();
 
+    /**
+     * Start display the retrieve invoice in the format we want
+     *
+     * @param info the invoice we retrieve, which in the format "delivered, issued, paid,
+     *     total price, item, price, quantity"
+     */
     void setInvoiceInfo(String[] info);
 
+    /**
+     * change paid from false to true in the xml
+     */
     void changePayTrue();
 
+    /**
+     * display the toast error message if there is an error in change pay boolean
+     */
     void changePayError();
 }
